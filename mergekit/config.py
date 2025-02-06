053dfde3e41e4a43b70c6f963e812596289b528d
Fizz~ <168598314+fizzAI@users.noreply.github.com>
2025-02-06 09:37:09 -0500

    Manually format 2

--- mergekit/config.py
+++ mergekit/config.py
@@ -90,9 +90,7 @@ class MergeConfiguration(BaseModel):
     parameters: Optional[Dict[str, ParameterSetting]] = None
     base_model: Optional[ModelReference] = None
     dtype: Optional[str] = None
-    tokenizer_source: Union[Literal["union"], Literal["base"], ModelReference] = (
-        "base"
-    )
+    tokenizer_source: Union[Literal["union"], Literal["base"], ModelReference] = "base"
     tokenizer: Optional[TokenizerConfig] = None
     chat_template: Optional[str] = None
     out_dtype: Optional[str] = None
